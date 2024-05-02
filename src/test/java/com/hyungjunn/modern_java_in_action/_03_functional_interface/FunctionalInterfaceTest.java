a10fe29609c282a17386bdf8140dc517a3164d69
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-05-02 13:20:16 +0900

    Test(_03_functional_interface): Add test for BiFunction functional interface

--- src/test/java/com/hyungjunn/modern_java_in_action/_03_functional_interface/FunctionalInterfaceTest.java
+++ src/test/java/com/hyungjunn/modern_java_in_action/_03_functional_interface/FunctionalInterfaceTest.java
@@ -1,5 +1,7 @@
 package com.hyungjunn.modern_java_in_action._03_functional_interface;
 
+import com.hyungjunn.modern_java_in_action._02_apples.Apple;
+import com.hyungjunn.modern_java_in_action._02_apples.Color;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.BiFunction;
@@ -21,6 +23,14 @@ public class FunctionalInterfaceTest {
         BiFunction<Integer, Integer, Integer> adder = (a, b) -> Integer.sum(a, b);
         int result = adder.apply(4, 5);
         assertThat(result).isEqualTo(9);
+    }
+
+    @Test
+    void testBiFunction3() {
+        // 두 사과의 무게를 비교하는 로직
+        BiFunction<Apple, Apple, Integer> adder = (apple1, apple2) -> Integer.max(apple1.getWeight(), apple2.getWeight());
+        int result = adder.apply(new Apple(300, Color.RED), new Apple(400, Color.RED));
+        assertThat(result).isEqualTo(400);
     }
 
     @Test
