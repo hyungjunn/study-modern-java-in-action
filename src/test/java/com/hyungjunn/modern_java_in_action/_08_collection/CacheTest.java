7c3417addcbd89f5c22d88ef4d52b51258a1c548
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-05-18 02:48:26 +0900

    add CacheExample#printByte

---
+++ src/test/java/com/hyungjunn/modern_java_in_action/_08_collection/CacheTest.java
@@ -0,0 +1,12 @@
+package com.hyungjunn.modern_java_in_action._08_collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+public class CacheTest {
+    @Test
+    void testCache() throws NoSuchAlgorithmException {
+        new CacheExample().printByte();
+    }
+}
